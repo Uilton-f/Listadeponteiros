79ca0f2e511b398076d9fa5c3ec0c3763c0b25c5
Uilton-f <115167587+Uilton-f@users.noreply.github.com>
2022-10-07 20:27:28 -0300

    Add files via upload

---
+++ Questao10.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void) {
+  char x[4], *pc;
+  int xi[4], i, *pi;
+  float xf[4], *pf;
+  double xd[4], *pd;
+
+  pc = &x;
+  pi = &xi;
+  pf = &xf;
+  pd = &xd;
+
+  //Assim como na questão 9 o espaço reservado na memória para o tipo char é de 1 byte
+  for(i = 1; i < 4; i++){
+    printf("endereço de x + %d = %X\n", i, x+i);
+  }
+
+  printf("\n");
+
+  //Devido à arquitetura da máquina o tipo int ocupa 4 bytes na memória, diferente da questão 9, na qual foi feita a suposição de ocupar apenas 2 bytes
+  for(i = 1; i < 4; i++){
+    printf("endereço de xi + %d = %X\n", i, xi+i);
+  }
+
+  printf("\n");
+
+  //Assim como na questão 9 o espaço reservado na memória para o tipo float é de 4 bytes
+  for(i = 1; i < 4; i++){
+    printf("endereço de xf + %d = %X\n", i, xf+i);
+  }
+
+  printf("\n");
+
+  //Assim como na questão 9 o espaço reservado na memória para o tipo double é de 8 bytes
+  for(i = 1; i < 4; i++){
+    printf("endereço de xd + %d = %X\n", i, xd+i);
+  }
+  return 0;
+}
