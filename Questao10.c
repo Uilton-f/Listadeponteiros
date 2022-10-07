72dc8e50eac415283cc63878418df8569d17a3a1
Uilton-f <115167587+Uilton-f@users.noreply.github.com>
2022-10-07 20:28:57 -0300

    Update Questao10.c

--- Questao10.c
+++ Questao10.c
@@ -38,3 +38,21 @@ int main(void) {
   }
   return 0;
 }
+
+//Exemplo de impressão dos resultados:
+
+endereço de x + 1 = A6F4AE6D
+endereço de x + 2 = A6F4AE6E
+endereço de x + 3 = A6F4AE6F
+
+endereço de xi + 1 = A6F4AE54
+endereço de xi + 2 = A6F4AE58
+endereço de xi + 3 = A6F4AE5C
+
+endereço de xf + 1 = A6F4AE44
+endereço de xf + 2 = A6F4AE48
+endereço de xf + 3 = A6F4AE4C
+
+endereço de xd + 1 = A6F4AE28
+endereço de xd + 2 = A6F4AE30
+endereço de xd + 3 = A6F4AE38
